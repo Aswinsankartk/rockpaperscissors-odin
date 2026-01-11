60b9c134e4d284986dfd3039e9bdc1d74bd4419c
Aswinsankartk <164378656+Aswinsankartk@users.noreply.github.com>
2026-01-11 10:32:37 +0000

    Add a basic GUI with buttons to play the game in viewport.
    Initially, it can only be played in the console. Now, results are viewed in the viewport.

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+//CREATING Ps AND DIVs FOR DISPLAYING THE ROUND RESULT, SCORE AND FINAL WINNER.
+const resultContainer=document.createElement("div");
+const roundResult=document.createElement("p");
+const score=document.createElement("p");
+const finalWinner=document.createElement("p");
+document.body.appendChild(resultContainer);
+resultContainer.append(roundResult,score,finalWinner);
+
+//SELECTING THE BUTTONS TO PLAY FROM THE DOM.
+const rockBtn=document.querySelector("#rock-btn");
+const paperBtn=document.querySelector("#paper-btn");
+const scissorsBtn=document.querySelector("#scissors-btn");
+
+/* ADDING EVENT LISTENERS & PASSING FUNCTIONS TO BUTTONS
+   AND CHANGING CONTENT IN THE DOM. */
+rockBtn.addEventListener("click",()=>{                     //ROCK BUTTON
+    playRound("rock",getComputerChoice());
+    roundResult.textContent=`${msg}`;
+    score.textContent=`YOU ${humanScore} - ${computerScore} COMPUTER`;
+    finalWinner.textContent=winner;
+});
+paperBtn.addEventListener("click",()=>{                    //PAPER BUTTON
+    playRound("paper",getComputerChoice());
+    roundResult.textContent=`${msg}`;
+    score.textContent=`YOU ${humanScore} - ${computerScore} COMPUTER`;
+    finalWinner.textContent=winner;
+});
+scissorsBtn.addEventListener("click",()=>{                 //SCISSORS BUTTON
+    playRound("scissors",getComputerChoice());
+    roundResult.textContent=`${msg}`;
+    score.textContent=`YOU ${humanScore} - ${computerScore} COMPUTER`;
+    finalWinner.textContent=winner;
+});
+
+/* INITIALIZING VARIABLES TO STORE HUMAN & COMPUTER SCORE, ROUND RESULT, 
+   WINNER AND FLAG FOR STOPPING THE GAME AFTER ANY PLAYER HITS 5 POINTS. */
+let humanScore=0;
+let computerScore=0;
+let gameOver=false;
+let msg="";
+let winner="";
+
+//FUNCTION FOR GENERATING COMPUTER CHOICE.
 function getComputerChoice(){
     let num=Math.random();
     let choice="";
@@ -10,65 +53,52 @@ function getComputerChoice(){
     }
     return choice;
 }
-function getHumanChoice(){
-    let choice=prompt("Enter your choice [rock/paper/scissors]: ");
-    return choice;
-}
-let humanScore=0;
-let computerScore=0;
-        
-function playGame(){
-    function playRound(humsc,comsc){
-        const humanChoice=humsc.toLowerCase();
-        const computerChoice=comsc;
-        if (humanChoice=="rock"){
-            if(computerChoice=="rock"){
-                console.log("Tie! Both chose Rock!");
-            } else if (computerChoice=="paper"){
-                console.log("You lose! Paper beats Rock!");
-                computerScore++;
-            } else {
-                console.log("You win! Rock beats Scissors!");
-                humanScore++;
-            }
-        } else if (humanChoice=="paper"){
-            if(computerChoice=="paper"){
-                console.log("Tie! Both chose Paper!");
-            } else if (computerChoice=="rock"){
-                console.log("You win! Paper beats Rock");
-                humanScore++;
-            } else {
-                console.log("You lose! Scissors beat Paper!");
-                computerScore++;
-            }
+
+//FUNCTION FOR PLAYING ROUNDS.
+function playRound(humsc,comsc){
+    if(gameOver) return;
+    const humanChoice=humsc.toLowerCase();
+    const computerChoice=comsc;
+    if (humanChoice=="rock"){
+        if(computerChoice=="rock"){
+            msg="Tie! Both chose Rock!";
+        } else if (computerChoice=="paper"){
+            msg="You lose! Paper beats Rock!";
+            computerScore++;
         } else {
-            if(computerChoice=="scissors"){
-                console.log("Tie! Both chose Scissors!");
-            } else if (computerChoice=="rock"){
-                console.log("You lose! Rock beats Scissors!");
-                computerScore++;
-            } else {
-                console.log("You win! Scissors beats Paper!");
-                humanScore++;
-            }
+            msg="You win! Rock beats Scissors!";
+            humanScore++;
         }
-        return humanScore;
-        return computerScore;
-    }
-    let rounds=5;
-    for(let i=1;i<=5;i++){
-        playRound(getHumanChoice(),getComputerChoice());
-        console.log(humanScore+" "+computerScore);
-    }
-    if(humanScore>computerScore){
-        winner="You are the winner!";
-    } else if (computerScore>humanScore){
-        winner="Computer is the winner!";
+    } else if (humanChoice=="paper"){
+        if(computerChoice=="paper"){
+            msg="Tie! Both chose Paper!";
+        } else if (computerChoice=="rock"){
+            msg="You win! Paper beats Rock";
+            humanScore++;
+        } else {
+            msg="You lose! Scissors beat Paper!";
+            computerScore++;
+        }
     } else {
-        winner="Both tied on the title!";
+        if(computerChoice=="scissors"){
+            msg="Tie! Both chose Scissors!";
+        } else if (computerChoice=="rock"){
+            msg="You lose! Rock beats Scissors!";
+            computerScore++;
+        } else {
+            msg="You win! Scissors beats Paper!";
+            humanScore++;
+        }
     }
-    return winner;
+    //BLOCK TO CHECK IF 5 POINTS HIT OR NOT. 
+    //IF HIT, STOP THE GAME.
+    if(humanScore===5 || computerScore===5){
+        gameOver=true;
+        if(humanScore===5){
+            winner="YOU ARE THE WINNER!";
+        } else {
+            winner="COMPUTER WON THE GAME!";
+        }
+    };
+    return humanScore,computerScore,msg,winner;
 }
-playGame();
-alert(winner);
-console.log(winner);
